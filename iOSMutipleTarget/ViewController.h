0c64ab63880a667937f25cb6c3711b04e649ce0e
Mukesh Lokare <mukeshlokare555@gmail.com>
2017-08-14 14:06:25 +0530

    Initial Commit

---
+++ iOSMutipleTarget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSMutipleTarget
+//
+//  Created by webwerks on 14/08/17.
+//  Copyright © 2017 smart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
